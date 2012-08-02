7b6e6c9bf58cb3a1af8161fc4985b01f24bea3d2
Ted Pak <powerpak006@gmail.com>
2012-08-01 22:52:37 -0400

    add google analytics

--- index.php
+++ index.php
@@ -272,5 +272,21 @@
   </script>
   <!-- End UserVoice widget -->
   
+  <!-- Begin Google Analytics -->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-33827672-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+  <!-- End Google Analytics -->
+  
 </body>
 </html>
