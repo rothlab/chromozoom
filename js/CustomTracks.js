ff12654a774abb1234b50238c16619598e70dd33
Theodore Pak <powerpak006@gmail.com>
2016-02-03 13:58:03 -0500

    bigbed caching also implemented

--- js/CustomTracks.js
+++ js/CustomTracks.js
@@ -1361,22 +1361,39 @@
       parse: function(lines) {
         var self = this,
           middleishPos = self.browserOpts.genomeSize / 2,
-          cache = new IntervalTree(floorHack(middleishPos), {startKey: 'start', endKey: 'end'});
-        self.data = {cache: cache, info: {}};
+          cache = new IntervalTree(floorHack(middleishPos), {startKey: 'start', endKey: 'end'}),
+          ajaxUrl = self.ajaxDir() + 'bigbed.php',
+          remote;
+        
+        remote = new RemoteTrack(cache, function(start, end, storeIntervals) {
+          range = self.chrRange(start, end);
+          $.ajax(ajaxUrl, {
+            data: {range: range, url: self.opts.bigDataUrl, density: 'pack'},
+            success: function(data) {
+              var lines = _.filter(data.split('\n'), function(l) { var m = l.match(/\t/g); return m && m.length >= 2; });
+              var intervals = _.map(lines, function(l) { return self.type('bed').parseLine.call(self, l); });
+              storeIntervals(intervals);
+            }
+          });
+        });
+        
+        self.data = {cache: cache, remote: remote};
         self.heights = {max: null, min: 15, start: 15};
         self.sizes = ['dense', 'squish', 'pack'];
         self.mapSizes = ['pack'];
         
-        // Get general info on the bigBed
-        $.ajax(this.ajaxDir() + 'bigbed.php', {
-          data: {url: this.opts.bigDataUrl},
+        // Get general info on the bigBed and setup the binning scheme for the RemoteTrack
+        $.ajax(ajaxUrl, {
+          data: { url: self.opts.bigDataUrl },
           success: function(data) {
             // Set maxFetchWindow to avoid overfetching data.
             if (!self.opts.maxFetchWindow) {
               var meanItemsPerBp = data.itemCount / self.browserOpts.genomeSize,
                 maxItemsToDraw = _.max(_.values(self.opts.drawLimit));
               self.opts.maxFetchWindow = maxItemsToDraw / meanItemsPerBp;
+              self.opts.optimalFetchWindow = Math.floor(self.opts.maxFetchWindow / 3);
             }
+            remote.setupBins(self.browserOpts.genomeSize, self.opts.optimalFetchWindow, self.opts.maxFetchWindow);
           }
         });
         
@@ -1390,39 +1407,22 @@
           bppp = (end - start) / width,
           range = this.chrRange(start, end);
         
-        function lineNum(d, set) {
+        function lineNum(d, setTo) {
           var key = bppp + '_' + density;
-          if (!_.isUndefined(set)) { 
+          if (!_.isUndefined(setTo)) { 
             if (!d.line) { d.line = {}; }
-            return (d.line[key] = set);
+            return (d.line[key] = setTo);
           }
           return d.line && d.line[key]; 
         }
         
-        function success(data) {
+        function parseDenseData(data) {
           var drawSpec = [], 
-            lines, intervals, calcPixInterval;
-          if (density == 'dense') {
-            lines = data.split(/\s+/g);
-            _.each(lines, function(line, x) { 
-              if (line != 'n/a' && line.length) { drawSpec.push({x: x, w: 1, v: parseFloat(line) * 1000}); } 
-            });
-          } else {
-            lines = _.filter(data.split('\n'), function(l) { var m = l.match(/\t/g); return m && m.length >= 2; });
-            intervals = _.map(lines, function(l) { return { data: self.type('bed').parseLine.call(self, l)}; });
-            // TODO: add() these to an IntervalTree cache
-            // CAUTION: we need to figure out how to dedupe things, as we may load the same features multiple times, unintentionally.
-            // To dedupe, we may use use the Bio::DB:BigBed strategy:
-            // "Because BED files don't actually use IDs, the ID is constructed from the feature's name (if any), chromosome coordinates, strand and block count."
-            // Then search() for all the intervals in the range, iff we had some cached data already.
-            
-            calcPixInterval = new CustomTrack.pixIntervalCalculator(start, width, bppp, density=='pack');
-            
-            // TODO: we need to add a lineNum function here as the last argument so that we can track lines assigned to previously rendered features
-            // Otherwise, features often break between tiles which is ugly and misleading
-            // See function lineNum(d, set) above in the bed format
-            drawSpec = self.type('bed').stackedLayout.call(self, intervals, width, calcPixInterval);
-          }
+            lines;
+          lines = data.split(/\s+/g);
+          _.each(lines, function(line, x) { 
+            if (line != 'n/a' && line.length) { drawSpec.push({x: x, w: 1, v: parseFloat(line) * 1000}); } 
+          });
           callback(drawSpec);
         }
         
@@ -1431,11 +1431,20 @@
         if (density != 'dense' && (end - start) > self.opts.maxFetchWindow) {
           callback({tooMany: true});
         } else {
-          // TODO: cache results so we aren't refetching the same regions over and over again.
-          $.ajax(this.ajaxDir() + 'bigbed.php', {
-            data: {range: range, url: this.opts.bigDataUrl, width: width, density: density},
-            success: success
-          });
+          if (density == 'dense') {
+            $.ajax(self.ajaxDir() + 'bigbed.php', {
+              data: {range: range, url: self.opts.bigDataUrl, width: width, density: density},
+              success: parseDenseData
+            });
+          } else {
+            self.data.remote.fetchAsync(start, end, function(intervals) {
+              var calcPixInterval, drawSpec;
+              if (intervals.tooMany) { return callback(intervals); }
+              calcPixInterval = new CustomTrack.pixIntervalCalculator(start, width, bppp, false);
+              drawSpec = self.type('bed').stackedLayout.call(self, intervals, width, calcPixInterval, lineNum);
+              callback(drawSpec);
+            });
+          }
         }
       },
     
@@ -1524,7 +1533,7 @@
         self.mapSizes = ['pack'];
         
         // Get general info on the bam (e.g. `samtools idxstats`, use mapped reads per reference sequence
-        // to estimate maxFetchWindow and optimalFetchWindow, and set this on the RemoteTrack.
+        // to estimate maxFetchWindow and optimalFetchWindow, and setup binning on the RemoteTrack.
         $.ajax(ajaxUrl, {
           data: {url: self.opts.bigDataUrl},
           success: function(data) {
